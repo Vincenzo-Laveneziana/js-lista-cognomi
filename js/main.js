6af1ebb9b36c92d5a5dbada93e672f112aa2b343
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-03-31 15:40:01 +0200

    implementazione first letter uppercase completata

--- js/main.js
+++ js/main.js
@@ -9,9 +9,13 @@
  var tuoCognome = prompt("inserisci il tuo cognome");
  console.log(tuoCognome);
 
+ //rendo il cognome dell'ultente first letter uppercase
+ var tuoCognomeUp = tuoCognome.charAt(0).toUpperCase() + tuoCognome.slice(1);
+
  //inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
  var cognomi = [" Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi", "Laveneziana"];
- cognomi.push(tuoCognome);
+
+ cognomi.push(tuoCognomeUp);
  console.log(cognomi);
 
  //stampa la lista ordinata alfabeticamente
@@ -21,7 +25,7 @@
  //scrivi anche la posizione della lista in cui  il nuovo utente si trova
 
  for( var i=0; i < cognomi.length; i++){
-  if(tuoCognome == cognomi[i]){
+  if(tuoCognomeUp == cognomi[i]){
     console.log("Il nuovo utente si trova nella posizione " + (i+1) + " della lista ordinata");
   }
  }
