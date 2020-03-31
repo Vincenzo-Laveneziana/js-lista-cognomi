9d42b4e83786f7b201da711da5c128d49d4343fd
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-03-31 19:27:05 +0200

    riscrittura file js completata

--- js/main2.js
+++ js/main2.js
@@ -4,39 +4,41 @@
 var tuoCognome = document.getElementById("tuo-cognome");
 var list = document.getElementById("list");
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi", "Laveneziana"];
-var btnCerca = document.getElementById("bottoneCerca");
 tuoCognome.focus();
 
-btnCerca.addEventListener("click", function(){
-
-    //first letter uppercase
+bottoneCerca.addEventListener("click", function(){
+  //first letter uppercase
   var tuoCognomeUp = tuoCognome.value.charAt(0).toUpperCase() + tuoCognome.value.slice(1);
 
   //inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
   cognomi.push(tuoCognomeUp);
   console.log(cognomi);
   
-  var items ="";
+  var elementi ="";
 
   //stampa la lista ordinata alfabeticamente
   cognomi.sort();
-  console.log(cognomi);
+  //console.log(cognomi);
 
   for (var i = 0; i < cognomi.length; i++) {
-    items += "<li>" + cognomi[i] + "</li>";
+    elementi += "<li>" + cognomi[i] + "</li>";
     if(tuoCognomeUp == cognomi[i]){
       console.log("Il nuovo utente si trova alla posizione " + (i+1) + " della lista ordinata");
       document.getElementById("eccoti2").innerHTML = (i+1);
     }
   }
-
   //output elementi
   eccoti.className = "show";
-  list.innerHTML= items;
+  list.innerHTML= elementi;
 
-  
-  
-  
-
+  tuoCognome.value = "";
+  tuoCognome.focus();
 
 });
+
+bottoneReset.addEventListener("click",
+  function(){
+    window.location.reload(); 
+  }
+)
+
