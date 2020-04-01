4b65e75f8131562a2f4e04af07c31fbd92c8f183
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-01 09:52:02 +0200

    fine esercizio in attesa di correzione

--- js/main.js
+++ js/main.js
@@ -1,53 +1,66 @@
+//referenze
+var i;
 
+//chiedere all’utente il cognome
+var tuoCognome = document.getElementById("tuo-cognome");
+var list = document.getElementById("list");
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi", "Laveneziana"];
+tuoCognome.focus();
 
-bottoneCerca.addEventListener("click",
-  function(){
-    //chiedere all’utente il cognome
-    var tuoCognome = document.getElementById("tuo-cognome");
-    console.log(tuoCognome);
+bottoneCerca.addEventListener("click", function(){
+  
+   //first letter uppercase
+  var tuoCognomeUp = tuoCognome.value.charAt(0).toUpperCase() + tuoCognome.value.slice(1);
+  var boolean = false;
+  
+  for ( i = 0; i < cognomi.length; i++ ){
+   
+    var controllo = cognomi[i];
+    console.log(controllo);
 
-    //rendo il cognome dell'ultente first letter uppercase
-    var tuoCognomeUp = tuoCognome.charAt(0).toUpperCase() + tuoCognome.slice(1);
-
-    if(cognomi.indexOf(tuoCognomeUp) < 0){
-
-      cognomi.push(tuoCognomeUp);
-      console.log(cognomi);
-
-      //stampa la lista ordinata alfabeticamente
-      cognomi.sort();
-      console.log(cognomi);
-
-
-      //scrivi anche la posizione della lista in cui  il nuovo utente si trova
-
-      var elLista = document.getElementById("list");
-
-      for( var i=0; i < cognomi.length; i++){
-
-        var elementoLista = cognomi[i];
-
-        var contenuto = elLista.innerHTML;
-
-        var nuovoElemento = "<li>" + elementoLista + "</li>"
-          elLista.innerHTML = contenuto + nuovoElemento;
-        
-        if(tuoCognomeUp == cognomi[i]){
-          console.log("Il nuovo utente si trova alla posizione " + (i+1) + " della lista ordinata");
-          document.getElementById("eccoti2").innerHTML = (i+1);
-        }
-
-      }
-
-    eccoti.className = "show";
-
-    } else{
-      alert("cognome già inserito");
+    if (tuoCognomeUp == controllo){
+      boolean = true;
     }
+  }
+
+  if ( boolean == true){
+    alert("Cognome già esistente");
+  } else if (boolean == false){
+      
+    //inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+    cognomi.push(tuoCognomeUp);
+    cognomi.sort();
+    console.log(cognomi);
+
+    var elementi ="";
 
 
+    for (i = 0; i < cognomi.length; i++) {
+      elementi += "<li>" + cognomi[i] + "</li>";
+
+      if(tuoCognomeUp == cognomi[i]){
+        console.log("Il nuovo utente si trova alla posizione " + (i+1) + " della lista ordinata");
+        document.getElementById("eccoti2").innerHTML = (i+1);
+      }
+    }
+    //output elementi
+    eccoti.className = "show";
+    list.innerHTML= elementi;
+
+    tuoCognome.value = "";
+    tuoCognome.valueUp = "";
+    tuoCognome.focus();
+  }
+    
+  
+
+  
+
+});
+
+bottoneReset.addEventListener("click",
+  function(){
+    window.location.reload(); 
   }
 )
-
 
